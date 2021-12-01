50dea5c9560421eabe05b4fed64a7e9a53047909
zlyang <565213263@qq.com>
2021-12-01 09:06:05 +0800

    init front admin-mall

---
+++ sale-mall-front/mall-admin-app/src/pages/goodsListPage.js
@@ -0,0 +1,15 @@
+
+
+import SideBar from "../components/sideBar";
+
+export default function GoodsList(props) {
+
+    return (
+        <div className='main-content'>
+            <SideBar />
+            <div className='content-box'>
+                <h2 className='content-header'>商品列表</h2>
+            </div>
+        </div>
+    );
+}
